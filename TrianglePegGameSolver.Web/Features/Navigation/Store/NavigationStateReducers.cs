de3edda91b74a9c7f3c5724310b2d4eea41af719
Tim Grannen <grannendev@gmail.com>
2021-06-01 11:06:40 -0400

    Add Navigation state to enable closing menu on navigation link click

---
+++ TrianglePegGameSolver.Web/Features/Navigation/Store/NavigationStateReducers.cs
@@ -0,0 +1,16 @@
+﻿using Fluxor;
+
+namespace TrianglePegGameSolver.Web.Features.Navigation.Store
+{
+    public static class NavigationStateReducers
+    {
+        [ReducerMethod]
+        public static NavigationState MoveMadeAction(NavigationState state, NavigationAction action)
+        {
+            return state with
+            {
+                Open = action.Open
+            };
+        }
+    }
+}
